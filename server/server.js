0e359da44a35313381277a51b0a1d11a479489b6
Myika Marshall <myika.marshall@gmail.com>
2021-12-27 14:26:33 -0600

    updated server to include details router

--- server/server.js
+++ server/server.js
@@ -3,6 +3,8 @@ const app = express();
 const bodyParser = require('body-parser');
 const movieRouter = require('./routes/movie.router.js')
 const genreRouter = require('./routes/genre.router.js')
+// adding a details router
+const detailsRouter = require('./routes/details.router.js')
 const port = process.env.PORT || 5000;
 
 /** ---------- MIDDLEWARE ---------- **/
@@ -12,6 +14,8 @@ app.use(express.static('build'));
 /** ---------- ROUTES ---------- **/
 app.use('/api/movie', movieRouter);
 app.use('/api/genre', genreRouter)
+// list added details router
+app.use('/api/details', detailsRouter);
 
 /** ---------- START SERVER ---------- **/
 app.listen(port, function () {
